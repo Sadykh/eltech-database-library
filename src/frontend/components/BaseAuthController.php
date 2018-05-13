7b252929d7f99cea0a2371bc5073d76cfcd427df
Sadykh <sadykh.sadykhov@ya.ru>
2018-05-13 22:13:05 +0300

    Исправление не для авторизованных

--- src/frontend/components/BaseAuthController.php
+++ src/frontend/components/BaseAuthController.php
@@ -9,7 +9,7 @@ class BaseAuthController extends Controller
     public function beforeAction($action)
     {
         if (parent::beforeAction($action)) {
-            if (\Yii::$app->user->isGuest) {
+            if (\Yii::$app->user->isGuest && $action = 'author') {
                 $this->redirect('/');
                 return false;
             }
